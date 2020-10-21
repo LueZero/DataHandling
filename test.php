6d6ac94ba95bc7b8942d09f6dc046239b767ed9f
zero <a0970937127@gmail.com>
2020-10-21 17:46:49 +0800

    202010211746 commit

--- test.php
+++ test.php
@@ -6,6 +6,9 @@ error_reporting(E_ALL);
 require_once __DIR__ . '/vendor/autoload.php';
 
 use Zero\DataHandling\DataHandling;
+
 $array = [1,2,3];
+
 $result = DataHandling::fill($array)->format(["key"])->map();
+
 echo $result;
